f079254fc54b51b26b3fab2f8162dc3a02b90cd1
ZigyTheBird <105124180+ZigyTheBird@users.noreply.github.com>
2025-07-07 22:55:47 +0330

    GeckoLib armor compat needs more love (removed)

--- common/src/main/java/com/zigythebird/bendable_cuboids/impl/compatibility/geckolib/GeoCubeAccessor.java
+++
@@ -1,8 +0,0 @@
-package com.zigythebird.bendable_cuboids.impl.compatibility.geckolib;
-
-import it.unimi.dsi.fastutil.Pair;
-
-public interface GeoCubeAccessor {
-    void setBendableCuboid(Pair<String, GeckoLibBendableCuboid> bendableCuboid);
-    Pair<String, GeckoLibBendableCuboid> getBendableCuboid();
-}
