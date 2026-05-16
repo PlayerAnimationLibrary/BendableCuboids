9918b78f69f8a2d81737549fc16c3a7a7dfd1c0c
dima_dencep <dima_dencep@redlance.org>
2026-05-17 02:54:21 +0700

    initial

--- src/fabric/java/com/zigythebird/bendable_cuboids/fabric/BendableCuboidsModFabric.java
+++ src/fabric/java/com/zigythebird/bendable_cuboids/fabric/BendableCuboidsModFabric.java
@@ -1,9 +1,11 @@
 package com.zigythebird.bendable_cuboids.fabric;
 
+import com.google.j2objc.annotations.J2ObjCIncompatible;
 import com.zigythebird.bendable_cuboids.BendableCuboidsMod;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
+@J2ObjCIncompatible
 public class BendableCuboidsModFabric extends BendableCuboidsMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
--- src/main/java/com/zigythebird/bendable_cuboids/BendableCuboidsMod.java
+++ src/main/java/com/zigythebird/bendable_cuboids/BendableCuboidsMod.java
@@ -1,9 +1,11 @@
 package com.zigythebird.bendable_cuboids;
 
+import com.google.j2objc.annotations.J2ObjCIncompatible;
 import com.zigythebird.bendable_cuboids.impl.compatibility.SkinLayersCompat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@J2ObjCIncompatible
 public abstract class BendableCuboidsMod {
     public static final Logger LOGGER = LoggerFactory.getLogger("BendableCuboids");
 
